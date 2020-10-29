c748c3bbbc2b5510fa0d3cdbbf5890cfec0e3249
Martin Röbke <martin.roebke@zeiss.com>
2020-10-29 15:19:21 +0100

    Refactor most parts

--- Labs/M02-StorageAccount/src/main/java/azure/schulung/MyUI.java
+++ Labs/M02-StorageAccount/src/main/java/azure/schulung/MyUI.java
@@ -57,258 +57,297 @@ public class MyUI extends UI {
     TextArea metaData = new TextArea("Metadata");
     private BlobServiceClient serviceClient;
 
-	Grid<BlobSignedIdentifier> bsi = new Grid<>("Access");
-
-	TextField url = new TextField("URL");
-
-	TextArea metaData = new TextArea("Metadata");
-	private BlobServiceClient serviceClient;
-
-	@Override
-	protected void init(VaadinRequest vaadinRequest) {
-
-		HttpClient client = new NettyAsyncHttpClientBuilder().build();
-		serviceClient = new BlobServiceClientBuilder().httpClient(client).connectionString(CONNECTION_KEY)
-				.buildClient();
-
-		final VerticalLayout layout = new VerticalLayout();
-
-		treeGrid.addColumn(Entry::getName).setCaption("Name");
-		treeGrid.addColumn(Entry::getAccessLevel).setCaption("AccessLevel");
-		treeGrid.addColumn(e -> e.accessPolicies).setCaption("Policies");
-		treeGrid.addColumn(e -> e.lastModified).setCaption("Modified");
-		treeGrid.addColumn(e -> e.accessTier).setCaption("Access Tier");
-		treeGrid.addColumn(e -> e.blobTier).setCaption("Blob Tier");
-		treeGrid.setWidth("100%");
-		treeGrid.addSelectionListener(e -> select(e));
-
-		bsi.addColumn(e -> e.getId()).setCaption("Id");
-
-		metaData.setWidth("100%");
-		bsi.setWidth("100%");
-
-		Button button = new Button("Update");
-		button.addClickListener(e -> this.update());
-
-		HorizontalLayout hl = new HorizontalLayout();
-
-		Button createSASB = new Button("createSAS", e -> createSAS());
-
-		hl.addComponents(button, new Button("Create Lease", e -> createLease()), createSASB);
-
-		url.setWidth("100%");
-		layout.addComponents(hl, url, treeGrid, metaData, bsi);
-
-		update();
-
-		setContent(layout);
-	}
-
-	private void createLease() {
-		Set<Entry> set = treeGrid.getSelectedItems();
-		if (!set.isEmpty()) {
-			Entry entry = set.iterator().next();
-
-			if (entry.blobcc != null) {
-				BlobLeaseClient blobLeaseClient = new BlobLeaseClientBuilder().containerClient(entry.blobcc)
-						.buildClient();
-				blobLeaseClient.acquireLease(60);
-				Notification.show("Lease Setted!");
-			} else if (entry.blobClient != null) {
-				BlobLeaseClient blobLeaseClient = new BlobLeaseClientBuilder().blobClient(entry.blobClient)
-						.buildClient();
-				blobLeaseClient.acquireLease(60);
-				Notification.show("Lease Setted!");
-			} else {
-				Notification.show("No blobcc");
-			}
-		} else {
-			Notification.show("No Item");
-		}
-	}
-
-	private void createSAS() {
-
-		Set<Entry> set = treeGrid.getSelectedItems();
-		if (set.isEmpty()) {
-			Notification.show("No selction");
-			return;
-		}
-		Entry entry = set.iterator().next();
-		if (entry.blobClient == null) {
-			Notification.show("No File selected.");
-			return;
-		}
-
-		String cname = entry.blobClient.getContainerName();
-		String fname = entry.blobClient.getBlobName();
-
-		BlobSasPermission blobPermission = new BlobSasPermission().setReadPermission(true);
-
-		BlobServiceSasSignatureValues builder = new BlobServiceSasSignatureValues().setProtocol(SasProtocol.HTTPS_ONLY)
-				.setExpiryTime(OffsetDateTime.now().plusDays(2)).setContainerName(cname).setBlobName(fname);
-
-		if (!bsi.getSelectedItems().isEmpty()) {
-
-			BlobSignedIdentifier identifier = bsi.getSelectedItems().iterator().next();
-			System.out.println("perm=" + identifier.getId());
-			builder = builder.setIdentifier(identifier.getId());
-		} else {
-			builder = builder.setPermissions(blobPermission);
-		}
-
-		System.out.println("" + entry.blobClient.getBlobUrl());
-
-		StorageSharedKeyCredential credential = new StorageSharedKeyCredential(STORAGE_ACCOUNT_NAME,
-				STORAGE_ACCOUNT_KEY);
-		BlobServiceSasQueryParameters sasQueryParameters = builder.generateSasQueryParameters(credential);
-		System.out.println(entry.blobClient.getBlobUrl() + "?" + sasQueryParameters.encode());
-
-		url.setValue(entry.blobClient.getBlobUrl() + "?" + sasQueryParameters.encode());
-	}
-
-	private Object select(SelectionEvent<Entry> e) {
-		if (e.getFirstSelectedItem().isPresent()) {
-			Entry entry = e.getFirstSelectedItem().get();
-			if (entry.metadata != null) {
-				String value = "";
-				for (String key : entry.metadata.keySet()) {
-					value += key + "=" + entry.metadata.get(key) + "\n";
-				}
-				metaData.setValue(value);
-			} else {
-				metaData.setValue("-" + e.getFirstSelectedItem().get().name);
-			}
-			if (entry.accessPolicies != null) {
-				bsi.setItems(entry.accessPolicies);
-			}
-		}
-		return null;
-	}
-
-	private void update() {
-		Entry root = new Entry();
-		root.setName("Root");
-		PagedIterable<BlobContainerItem> listContainer = serviceClient.listBlobContainers();
-		for (BlobContainerItem container : listContainer) {
-			Entry containerEntry = new Entry();
-			containerEntry.setName(container.getName());
-			root.getChildren().add(containerEntry);
-			container.getProperties().getPublicAccess();
-			BlobContainerClient blobcc = serviceClient.getBlobContainerClient(container.getName());
-
-			PublicAccessType access = blobcc.getAccessPolicy().getBlobAccessType();
-			containerEntry.blobcc = blobcc;
-
-			containerEntry.setMetadata(blobcc.getProperties().getMetadata());
-
-			containerEntry.accessPolicies = blobcc.getAccessPolicy().getIdentifiers();
-
-			PagedIterable<BlobItem> blobList = blobcc.listBlobs();
-
-			for (BlobItem blobitem : blobList) {
-				Entry blobEntry = new Entry();
-				blobEntry.setName(blobitem.getName());
-				blobEntry.setBlobItem(blobitem);
-				blobEntry.lastModified = blobitem.getProperties().getLastModified().toOffsetTime();
-				blobEntry.accessTier = blobitem.getProperties().getAccessTier().toString();
-				blobEntry.blobTier = blobitem.getProperties().getBlobType().toString();
-
-				containerEntry.getChildren().add(blobEntry);
-				blobEntry.setMetadata(blobitem.getMetadata());
-				blobEntry.blobClient = blobcc.getBlobClient(blobitem.getName());
-
-			}
-
-		}
-
-		treeGrid.setItems(root.getChildren(), Entry::getChildren);
-
-	}
-
-	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-	public static class MyUIServlet extends VaadinServlet {
-	}
-
-	class Entry {
-		public BlobContainerClient blobcc;
-
-		public List<BlobSignedIdentifier> accessPolicies;
-
-		public String blobTier;
-
-		public String accessTier;
-
-		public OffsetTime lastModified;
-
-		public BlobClient blobClient;
-
-		String name = "";
-
-		String level = "";
-
-		String policy = "";
-
-		String accessLevel = "";
-
-		Map<String, String> metadata;
-
-		public void setAccessLevel(String accessLevel) {
-			this.accessLevel = accessLevel;
-		}
-
-		public void setMetadata(Map<String, String> metadata) {
-			this.metadata = metadata;
-
-		}
-
-		public String getAccessLevel() {
-			return this.accessLevel;
-		}
-
-		public BlobItem getBlobItem() {
-			return mBlobItem;
-		}
-
-		public void setBlobItem(BlobItem mBlobItem) {
-			this.mBlobItem = mBlobItem;
-		}
-
-		BlobItem mBlobItem;
-
-		List<Entry> children = new ArrayList<>();
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getLevel() {
-			return level;
-		}
-
-		public void setLevel(String level) {
-			this.level = level;
-		}
-
-		public String getPolicy() {
-			return policy;
-		}
-
-		public void setPolicy(String policy) {
-			this.policy = policy;
-		}
-
-		public List<Entry> getChildren() {
-			return children;
-		}
-
-		public void setChildren(List<Entry> children) {
-			this.children = children;
-		}
-	}
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+        HttpClient client = new NettyAsyncHttpClientBuilder().build();
+        serviceClient = new BlobServiceClientBuilder().httpClient(client).connectionString(CONNECTION_KEY)
+                .buildClient();
+
+        final VerticalLayout layout = new VerticalLayout();
+
+        treeGrid.addColumn(Entry::getName).setCaption("Name");
+        treeGrid.addColumn(Entry::getAccessLevel).setCaption("AccessLevel");
+        treeGrid.addColumn(e -> e.accessPolicies).setCaption("Policies");
+        treeGrid.addColumn(e -> e.lastModified).setCaption("Modified");
+        treeGrid.addColumn(e -> e.accessTier).setCaption("Access Tier");
+        treeGrid.addColumn(e -> e.blobTier).setCaption("Blob Tier");
+        treeGrid.setWidth("100%");
+        treeGrid.addSelectionListener(this::select);
+
+        bsi.addColumn(BlobSignedIdentifier::getId).setCaption("Id");
+
+        metaData.setWidth("100%");
+        bsi.setWidth("100%");
+
+        Button button = new Button("Update");
+        button.addClickListener(e -> this.update());
+
+        HorizontalLayout hl = new HorizontalLayout();
+
+        Button createSASB = new Button("createSAS", e -> createSAS());
+
+        hl.addComponents(button, new Button("Create Lease", e -> createLease()), createSASB);
+
+        url.setWidth("100%");
+        layout.addComponents(hl, url, treeGrid, metaData, bsi);
+
+        update();
+
+        setContent(layout);
+    }
+
+    private void createLease() {
+        Set<Entry> set = treeGrid.getSelectedItems();
+        if (!set.isEmpty()) {
+            Entry entry = set.iterator().next();
+
+            if (entry.getBlobContainerClient() != null) {
+                BlobLeaseClient blobLeaseClient = new BlobLeaseClientBuilder()
+                        .containerClient(entry.getBlobContainerClient())
+                        .buildClient();
+                blobLeaseClient.acquireLease(60);
+                Notification.show("Lease Setted!");
+            } else if (entry.blobClient != null) {
+                BlobLeaseClient blobLeaseClient = new BlobLeaseClientBuilder().blobClient(entry.blobClient)
+                        .buildClient();
+                blobLeaseClient.acquireLease(60);
+                Notification.show("Lease Setted!");
+            } else {
+                Notification.show("No blobcc");
+            }
+        } else {
+            Notification.show("No Item");
+        }
+    }
+
+    private void createSAS() {
+
+        Set<Entry> set = treeGrid.getSelectedItems();
+        if (set.isEmpty()) {
+            Notification.show("No selction");
+            return;
+        }
+        Entry entry = set.iterator().next();
+        if (entry.blobClient == null) {
+            Notification.show("No File selected.");
+            return;
+        }
+
+        String cname = entry.blobClient.getContainerName();
+
+        BlobSasPermission blobPermission = new BlobSasPermission().setReadPermission(true);
+
+        BlobServiceSasSignatureValues builder = new BlobServiceSasSignatureValues().setProtocol(SasProtocol.HTTPS_ONLY)
+                .setExpiryTime(OffsetDateTime.now().plusDays(2)).setContainerName(cname)
+                .setBlobName(entry.blobClient.getBlobName());
+
+        if (!bsi.getSelectedItems().isEmpty()) {
+
+            BlobSignedIdentifier identifier = bsi.getSelectedItems().iterator().next();
+            System.out.println("perm=" + identifier.getId());
+            builder.setIdentifier(identifier.getId());
+        } else {
+            builder.setPermissions(blobPermission);
+        }
+
+        System.out.println("" + entry.blobClient.getBlobUrl());
+
+        StorageSharedKeyCredential credential = new StorageSharedKeyCredential(STORAGE_ACCOUNT_NAME,
+                STORAGE_ACCOUNT_KEY);
+        BlobServiceSasQueryParameters sasQueryParameters = builder.generateSasQueryParameters(credential);
+        System.out.println(entry.blobClient.getBlobUrl() + "?" + sasQueryParameters.encode());
+
+        url.setValue(entry.blobClient.getBlobUrl() + "?" + sasQueryParameters.encode());
+    }
+
+    private void select(SelectionEvent<Entry> e) {
+        if (e.getFirstSelectedItem().isPresent()) {
+            Entry entry = e.getFirstSelectedItem().get();
+            if (entry.metadata != null) {
+                StringBuilder value = new StringBuilder();
+                for (String key : entry.metadata.keySet()) {
+                    value.append(key).append("=").append(entry.metadata.get(key)).append("\n");
+                }
+                metaData.setValue(value.toString());
+            } else {
+                metaData.setValue("-" + e.getFirstSelectedItem().get().name);
+            }
+            if (entry.accessPolicies != null) {
+                bsi.setItems(entry.accessPolicies);
+            }
+        }
+    }
+
+    private void update() {
+        Entry root = new Entry();
+        root.setName("Root");
+        PagedIterable<BlobContainerItem> listContainer = serviceClient.listBlobContainers();
+        for (BlobContainerItem container : listContainer) {
+            Entry containerEntry = new Entry();
+            containerEntry.setName(container.getName());
+            root.getChildren().add(containerEntry);
+            container.getProperties().getPublicAccess();
+            BlobContainerClient blobcc = serviceClient.getBlobContainerClient(container.getName());
+
+            containerEntry.setBlobContainerClient(blobcc);
+
+            containerEntry.setMetadata(blobcc.getProperties().getMetadata());
+
+            containerEntry.accessPolicies = blobcc.getAccessPolicy().getIdentifiers();
+
+            PagedIterable<BlobItem> blobList = blobcc.listBlobs();
+
+            for (BlobItem blobitem : blobList) {
+                Entry blobEntry = new Entry().setName(blobitem.getName()).setBlobItem(blobitem)
+                        .setLastModified(blobitem.getProperties().getLastModified().toOffsetTime())
+                        .setAccessTier(blobitem.getProperties().getAccessTier().toString())
+                        .setBlobTier(blobitem.getProperties().getBlobType().toString());
+
+                containerEntry.getChildren().add(blobEntry);
+                blobEntry.setMetadata(blobitem.getMetadata());
+                blobEntry.blobClient = blobcc.getBlobClient(blobitem.getName());
+            }
+        }
+
+        treeGrid.setItems(root.getChildren(), Entry::getChildren);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+
+    static class Entry {
+        BlobContainerClient blobContainerClient;
+        List<BlobSignedIdentifier> accessPolicies;
+        String blobTier;
+        String accessTier;
+        OffsetTime lastModified;
+        BlobClient blobClient;
+        String name = "";
+        String level = "";
+        String policy = "";
+        String accessLevel = "";
+        Map<String, String> metadata;
+        BlobItem blobItem;
+        List<Entry> children = new ArrayList<>();
+
+        public BlobContainerClient getBlobContainerClient() {
+            return blobContainerClient;
+        }
+
+        public Entry setBlobContainerClient(BlobContainerClient blobContainerClient) {
+            this.blobContainerClient = blobContainerClient;
+            return this;
+        }
+
+        public List<BlobSignedIdentifier> getAccessPolicies() {
+            return accessPolicies;
+        }
+
+        public Entry setAccessPolicies(List<BlobSignedIdentifier> accessPolicies) {
+            this.accessPolicies = accessPolicies;
+            return this;
+        }
+
+        public String getBlobTier() {
+            return blobTier;
+        }
+
+        public Entry setBlobTier(String blobTier) {
+            this.blobTier = blobTier;
+            return this;
+        }
+
+        public String getAccessTier() {
+            return accessTier;
+        }
+
+        public Entry setAccessTier(String accessTier) {
+            this.accessTier = accessTier;
+            return this;
+        }
+
+        public OffsetTime getLastModified() {
+            return lastModified;
+        }
+
+        public Entry setLastModified(OffsetTime lastModified) {
+            this.lastModified = lastModified;
+            return this;
+        }
+
+        public BlobClient getBlobClient() {
+            return blobClient;
+        }
+
+        public Entry setBlobClient(BlobClient blobClient) {
+            this.blobClient = blobClient;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Entry setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public String getLevel() {
+            return level;
+        }
+
+        public Entry setLevel(String level) {
+            this.level = level;
+            return this;
+        }
+
+        public String getPolicy() {
+            return policy;
+        }
+
+        public Entry setPolicy(String policy) {
+            this.policy = policy;
+            return this;
+        }
+
+        public String getAccessLevel() {
+            return accessLevel;
+        }
+
+        public Entry setAccessLevel(String accessLevel) {
+            this.accessLevel = accessLevel;
+            return this;
+        }
+
+        public Map<String, String> getMetadata() {
+            return metadata;
+        }
+
+        public Entry setMetadata(Map<String, String> metadata) {
+            this.metadata = metadata;
+            return this;
+        }
+
+        public BlobItem getBlobItem() {
+            return blobItem;
+        }
+
+        public Entry setBlobItem(BlobItem blobItem) {
+            this.blobItem = blobItem;
+            return this;
+        }
+
+        public List<Entry> getChildren() {
+            return children;
+        }
+
+        public Entry setChildren(List<Entry> children) {
+            this.children = children;
+            return this;
+        }
+    }
 }
